696b41e438adee5329396def1725c081b774f085
Aryan Mahajan <contact.aryan.mahajan@gmail.com>
2024-12-18 09:18:27 +0800

    "Version 2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aes_package",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[],
     description="A simple AES encryption and decryption module",
