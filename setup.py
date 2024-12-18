1d9c4106974c1a2e713bce8fbb9bb96d7ed32913
Aryan Mahajan <contact.aryan.mahajan@gmail.com>
2024-12-18 09:13:35 +0800

    "Updated README.MD to be more extensive"

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-# aes_package/setup.py
 from setuptools import setup, find_packages
+
+# Read long description from README.md (optional but recommended)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="aes_package",
@@ -7,7 +10,19 @@ setup(
     packages=find_packages(),
     install_requires=[],
     description="A simple AES encryption and decryption module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Ensures correct markdown format
     author="Aryan Mahajan",
     author_email="contact.aryan.mahajan@gmail.com",
     url="https://github.com/yourusername/aes_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # You can change this depending on your license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Adjust this based on the versions you support
+    extras_require={
+        "dev": ["pytest"],  # Add pytest for testing
+    },
+    test_suite="tests",  # Set the test suite directory for automatic discovery
 )
